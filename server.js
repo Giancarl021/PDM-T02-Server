31d5b8b8c38d744fb7d0d9208e31d7b2d6c15d2a
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-05-19 09:59:27 -0300

    Fixed loading of ENV Variables

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { build as buildEnvironment } from './src/services/environment.js';
 import app from './src/app.js';
 
-const port = process.env.PORT || 80;
-
-buildEnvironment().then(() => app.listen(port, () => console.log(`Listening on port ${port}`)));
+buildEnvironment().then(() => {
+    const port = process.env.PORT || 80;
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+});
